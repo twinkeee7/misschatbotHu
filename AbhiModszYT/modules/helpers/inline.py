e17e893acc9ba4e0d69c695f3bce53b237596139
Rolex <167498105+Sunnykumar1122@users.noreply.github.com>
2024-05-23 11:10:32 +0530

    Update inline.py

--- AbhiModszYT/modules/helpers/inline.py
+++ AbhiModszYT/modules/helpers/inline.py
@@ -108,7 +108,7 @@ HELP_BUTN = [
 
 ABOUT_BTN = [
     [
-        InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_GRP}"),
+        InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/FUKRA_DEMON"),
         InlineKeyboardButton(text="ʜᴇʟᴘ", callback_data="HELP"),
     ],
     [
@@ -116,7 +116,7 @@ ABOUT_BTN = [
         InlineKeyboardButton(text="sᴏᴜʀᴄᴇ", callback_data="SOURCE"),
     ],
     [
-        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/{UPDATE_CHNL}"),
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/{Babe_bot_update}"),
         InlineKeyboardButton(text="◁", callback_data="BACK"),
     ],
 ]
