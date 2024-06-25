9e916b7453ca9d17e63e3c6ce686545f80fc1746
twinkeee7 <167674604+twinkeee7@users.noreply.github.com>
2024-06-26 03:25:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,50 +9,29 @@ API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", "6718764653"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "@Riseeuniversee")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "@Riseeuniversee")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "@SupremeXlevel")
 
 # Random Start Images
 IMG = [
-    "https://telegra.ph/file/00eb565274ccbffcf149d.jpg",
-    "https://telegra.ph/file/396e2af77c4664164de18.jpg",
-    "https://telegra.ph/file/6f92a0e943d68a15169c0.jpg",
-    "https://telegra.ph/file/d49fbf4c00f839641afe3.jpg",
-    "https://telegra.ph/file/cdcdceb3d4fc34675b815.jpg",
-    "https://telegra.ph/file/cc6259af790c13f98c38c.jpg",
-    "https://telegra.ph/file/ffa18225730df716d3532.jpg",
-    "https://telegra.ph/file/dc051f4160954159675a2.jpg",
-    "https://telegra.ph/file/e0e0a3f2addbf58b943e8.jpg",
-    "https://telegra.ph/file/2e6d9db59c537c4521440.jpg",
-    "https://telegra.ph/file/f1951920bbd57921d8820.jpg",
-    "https://telegra.ph/file/ece475f9a419442c18f1d.jpg",
-    "https://telegra.ph/file/99a0014129f08eb1a44dc.jpg",
-    "https://telegra.ph/file/ac8e3751509cf4e1b4756.jpg",
-    "https://telegra.ph/file/e07ef19b1f9bbde9909ad.jpg",
-    "https://telegra.ph/file/d3ee41261ed7779f30a89.jpg",
+    "https://graph.org/file/b94fa0a71c5e9f8b53382.jpg",
+    "https://graph.org/file/b2146698ae7b91efab281.jpg",
+    "https://graph.org/file/ae47ae84e134bfaaf56d8.jpg",
+    "https://graph.org/file/39dc33af8d49a555824af.jpg",
+    "https://graph.org/file/ca07e2c89c1158637ed39.jpg",
 ]
 
 
 # Random Stickers
 STICKER = [
-    "CAACAgEAAxkBAAIJomRdLhVJVebkx0JRsp1STwTv3t3eAAJrAgAClpxhRD4z4bgqlIF0LwQ",
-    "CAACAgUAAxkBAAIJo2RdLhjLjCpmPipMT8ksrqwUjGAIAAK1BQACLZ8oVFVNmhalU8eOLwQ",
-    "https://t.me/addstickers/qdee8cWX_by_stickerskibot"
-    "https://t.me/addstickers/a6718764653_by_Japanesexmanagement_bot"
+    "https://t.me/addstickers/a_6718764653_by_Toga_Robot"
     ,
 ]
 
 
 EMOJIOS = [
-    "🎲",
-    "🔥",
-    "⚡️",
-    "⛈",
-    "🌩",
-    "🌦",
-    "☀️",
-    "💫",
-    "🐳",
-    "🦑",
+    "🙈",
+    "💗",
+    "💭",
 ]
